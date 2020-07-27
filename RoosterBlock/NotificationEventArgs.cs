6394d5b70896d403d830074711297217f7640b37
Rob Bernstein <robernst@microsoft.com>
2020-07-27 16:09:03 -0400

    Enabled notifications in the app. - Shows on status bar. - Shows in pull-down menu. - Still need to display a heads-up notification, hopefully in next commit. - Renamed Android app from "RoosterBlock.Android" to "Rooster Block" so it appears correctly in notifications.

---
+++ RoosterBlock/NotificationEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RoosterBlock
+{
+    public class NotificationEventArgs : EventArgs
+    {
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+    }
+}
